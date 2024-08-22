6dd62d058fb49cc85a1f511c01da258e21bf86eb
eshwar23 <enigma.bluewater@gmail.com>
2024-08-22 13:16:37 +0530

    Add files via upload

---
+++ DDAQuestions.java
@@ -0,0 +1,62 @@
+/*#
+Think in terms of position (index) of the values - i,j-are the row and column positions
+Think is terms of "THE POSITION OF THE REQUIRED VALUES"
+Program 1
+Write a program to create matrix in n*n matrix and perform the following tasks
+task 1: print the matrix in square matrix form
+task 2: print the sum of even values of left diagonal
+task 3: print the product of odd values of right diagonal values
+
+Program 2
+Write a program to create matrix in n*n matrix and perform the following tasks
+task 1: print the matrix in square matrix form
+task 2: print the sum of all the buzz numbers  and the frequency of buzz numbers present the matrix
+
+Program 3
+Write a program to create matrix in n*n matrix and perform the following tasks
+task 1: print the matrix in square matrix form
+task 2: print the sum of boundary values of the matrix
+
+Program 4
+Write a program to create matrix in n*n matrix and perform the following tasks
+task 1: print the matrix in square matrix form
+task 2: print the sum of each row along with the row number
+
+Program 5
+Write a program to create matrix in n*n matrix and perform the following tasks
+task 1: print the matrix in square matrix form
+task 2: print the sum of first and last row 
+
+RECTANGULAR MATRIX
+
+Program 6
+Write a program to create matrix in m*n matrix and perform the following tasks
+task 1: print the matrix in rectangular matrix form
+task 2: print the sum of each row along with the row number
+
+Program 7
+Write a program to create matrix in m*n matrix and perform the following tasks
+task 1: print the matrix in rectangular matrix form
+task 2: print the maximum value and minimum value of each row along with the row number
+
+Program 8
+Write a program to create matrix in m*n matrix and perform the following tasks
+task 1: print the matrix in rectangular matrix form
+task 2: print the sum of first and last column values
+
+Program 9
+Write a program to create matrix in m*n matrix and perform the following tasks
+task 1: print the matrix in rectangular matrix form
+task 2: print the sum of non boundary values of the matrix
+
+Program 10
+Write a program to create matrix in m*n matrix and perform the following tasks
+task 1: print the matrix in rectangular matrix form
+task 2: print the maximum and minimum of the matrix along with their position
+
+Program 11
+Write a program to create matrix in n*n matrix and perform the following tasks
+task 1: print the matrix in square matrix form
+task 2: print the greatest values of left diagonal
+task 3: print the smallest value of right diagonal 
+ */
